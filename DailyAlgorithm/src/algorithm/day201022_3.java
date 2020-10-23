f28c3fdbbdb828fc4ab742202eb539e18aa3be4c
alikwon <63032589+alikwon@users.noreply.github.com>
2020-10-24 02:07:48 +0900

    프로그래머스 알고리즘(정수 제곱근 판별)

---
+++ DailyAlgorithm/src/algorithm/day201022_3.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+/*
+●정수 제곱근 판별
+	문제 설명
+		임의의 양의 정수 n에 대해, 
+		n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+		n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, 
+		n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+	제한 사항
+		-n은 1이상, 50000000000000 이하인 양의 정수입니다.
+*/
+public class day201022_3 {
+
+	public static long solution(long n) {
+		if(n==1)
+            return 4;
+        for(long i=1;i<=n/2;i++){
+            if(i*i==n){
+                return (i+1)*(i+1);
+            }
+        }
+        return -1;
+	}
+	public static void main(String[] args) {
+		long n =51076;
+		System.out.println(solution(n));
+	}
+
+}
