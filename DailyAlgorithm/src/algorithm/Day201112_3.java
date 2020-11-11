8003534bfa37081560e806c467da57b8eb53a56c
alikwon <63032589+alikwon@users.noreply.github.com>
2020-11-12 03:59:52 +0900

    백준 알고리즘(소수찾기)

---
+++ DailyAlgorithm/src/algorithm/Day201112_3.java
@@ -0,0 +1,52 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*
+● 문자열 반복
+
+[문제]
+주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+[입력]
+첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 
+다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+
+[출력]
+주어진 수들 중 소수의 개수를 출력한다.
+
+*/
+public class Day201112_3 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		int answer = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			if (chkPrimeNum(n))
+				answer++;
+		}
+		br.close();
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
+
+	private static boolean chkPrimeNum(int n) {
+		if (n < 2) {
+			return false;
+		}
+		for (int i = 2; i <= n / 2; i++) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+}
